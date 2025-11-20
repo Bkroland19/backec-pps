81e74a4502445e8c00e1e5f4182d4eff697f92c3
codecatalystlabs <codecatalystug@gmail.com>
2025-11-20 16:22:41 +0300

    api localhost
    Co-Authored-By: Patrick Ayebazibwe <104129127+Patrck23@users.noreply.github.com>
    Co-Authored-By: Josh <48190147+Joshmyles@users.noreply.github.com>
    Co-Authored-By: bukenya kizza roland <65455750+Bkroland19@users.noreply.github.com>

--- docs/docs.go
+++ docs/docs.go
@@ -3139,7 +3139,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "csf.health.go.ug",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Point Prevalence Survey API",
